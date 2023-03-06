cd6e54a0fd9bd8a26ff23d7e498a2486044e5253
N1xvierenzeventig <111287243+N1xvierenzeventig@users.noreply.github.com>
2023-03-06 17:15:24 +0100

    Add files via upload

---
+++ Random name +Iq.py
@@ -0,0 +1,13 @@
+number = int(input("Input your number which you want to be cubed."))
+number2 = int(input("Input the number which you want to be squared."))
+
+def function(number, number2):
+    a = number*number*number#
+    b = number2*number2
+    return a+b
+
+
+print(function(number, number2))
+
+
+
